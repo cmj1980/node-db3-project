4654fdafefade39598995b43329272db15f8365e
cmj1980 <dagoclmt@gmail.com>
2022-06-17 06:47:55 -0700

    middleware complete working on model

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config')
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +8,22 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const schemeId = await db('schemes')
+      .where('scheme_id', req.params.scheme_id)
+      .first()
+    if (!schemeId) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`
+      })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
@@ -19,7 +35,20 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const {
+    scheme_name
+  } = req.body
+  if (scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ) {
+    next({
+      status: 400,
+      message: 'invalid scheme_name'
+    })
+  } else {
+    next()
+  }
 }
 
 /*
@@ -32,7 +61,24 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  const {
+    instructions,
+    step_number
+  } = req.body
+  if (instructions === undefined ||
+    typeof instrcutions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ) {
+    const err = {
+      status: 400,
+      message: 'invalid step'
+    }
+    next(err)
+  } else {
+    next()
+  }
 }
 
 module.exports = {
